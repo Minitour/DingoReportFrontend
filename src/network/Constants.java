2b67984cc08db70093a5bea84161e5377f4a6376
Tony Zaitoun <tony.z.1711@gmail.com>
2018-02-21 21:31:40 +0200

    added config file

--- src/network/Constants.java
+++ src/network/Constants.java
@@ -10,17 +10,17 @@ final public class Constants {
     /**
      * Protocol
      */
-    public final static String protocol = "http";
+    public final static String protocol = Config.config.get("protocol").getAsString();
 
     /**
      * Address
      */
-    public final static String address = "localhost";
+    public final static String address = Config.config.get("address").getAsString();
 
     /**
      * Port
      */
-    public final static String port = "8080";
+    public final static String port = Config.config.get("port").getAsString();
 
     /**
      * @return full path
